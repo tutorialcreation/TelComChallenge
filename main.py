0600c03a1b4493a90acbb3426d8597e7d337a9f1
tutorialcreation <tutorialcreation@gmail.com>
2022-05-11 07:05:49 +0300

    added top x by y column

--- main.py
+++ main.py
@@ -14,18 +14,25 @@ numeric_transformation = analyzer.generate_transformation(numeric_pipeline,"nume
 numeric_df = analyzer.frame_transforms(numeric_transformation,numerical_features)
 # measures of location
 st.sidebar.subheader("Measures of location")
-numeric_variable= st.sidebar.selectbox('Numeric Variables:',numerical_features)
+numeric_variable= st.sidebar.selectbox('Numeric Variables (x1):',numerical_features)
 numeric_variable_ = analyzer.map_index_to_feature(numeric_variable,numerical_features)
-categorical_variable = st.sidebar.selectbox('Categorical Variables:',categorical_features)
+numeric_variable_1= st.sidebar.selectbox('Numeric Variables (x2):',numerical_features)
+numeric_variable_1_ = analyzer.map_index_to_feature(numeric_variable,numerical_features)
+categorical_variable = st.sidebar.selectbox('Categorical Variables (x1):',categorical_features)
 categorical_variable_ = analyzer.map_index_to_feature(categorical_variable,categorical_features)
+categorical_variable_1 = st.sidebar.selectbox('Categorical Variables (x2):',categorical_features)
+categorical_variable_1_ = analyzer.map_index_to_feature(categorical_variable,categorical_features)
 top_x = int(st.sidebar.text_input("top x",3))
+top_y = int(st.sidebar.text_input("top y",5))
 
 if top_x and categorical_variable_:
     fig,values = analyzer.top_x_column(top_x,categorical_variable,"purple",online=True)
     st.pyplot(fig)
 
+if top_x and categorical_variable_ and top_y:
+    values = analyzer.top_x_by_y_cols(categorical_variable,categorical_variable_1,top_x,top_y)
+    st.write(values)
 
-values_ = analyzer.top_x_by_y_cols('Handset Manufacturer','Handset Type',3,5)
 aggregations = analyzer.aggregation_cols('MSISDN/Number','Total UL (Bytes)',True)
 analysis_1 = analyzer.non_graphical_analysis(numerical_features,"univariate",3)
 analysis_2 = analyzer.graphical_analysis(numerical_features,"univariate","curve",x=1)
