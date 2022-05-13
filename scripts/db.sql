74bb3eaacae6740f2d1edc2356acab3979f7d1c4
tutorialcreation <tutorialcreation@gmail.com>
2022-05-13 08:31:29 +0300

    added database modeling to application

---
+++ scripts/db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE userData(
+    id INT AUTO_INCREMENT,
+    customer FLOAT,
+    experience_score FLOAT,
+    engagement_score FLOAT,
+    satisfaction_sum FLOAT,
+    satisfaction_score FLOAT,
+    satisfaction_prediction_scores FLOAT,
+    class INT,
+    PRIMARY KEY(id)
+);
